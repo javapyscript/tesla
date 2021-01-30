560ea9db711db152898d73396de512b215e7bf17
chaitanya.bangera <chaitanya.bangera@genesiscare.co.uk>
2021-01-30 11:23:13 +0000

    test

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.scss';
+
+
+const Navbar = (): JSX.Element => {
+    return (
+        <div className="navbar"></div>
+    );
+}
+
+export default Navbar;
