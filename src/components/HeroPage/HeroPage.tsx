21ad0052fc6be74590eb29f9f071c0c9b4562062
chaitanya.bangera <chaitanya.bangera@genesiscare.co.uk>
2021-02-10 16:32:35 +0000

    responsive header

--- src/components/HeroPage/HeroPage.tsx
+++ src/components/HeroPage/HeroPage.tsx
@@ -7,24 +7,28 @@ interface HeroProps {
     index: number;
 }
 
+const wallpapers = [
+
+];
+
 const pages = [
     {
-        title: "Model 3", description: "0% Company Car Tax. <u>Learn More</u>", buttons: ["Custom Order", "Available Inventory"], secAction: "Schedule a Virtual Consultation"
+        title: "Model 3", description: "0% Company Car Tax. <u>Learn More</u>", buttons: ["Custom Order", "Available Inventory"], secAction: "Schedule a Virtual Consultation", wallRoot: "model3",
     }, 
     {
-        title: "Model S", description: "0% Company Car Tax.", buttons: ["Custom Order", "Available Inventory"], secAction: null
+        title: "Model S", description: "0% Company Car Tax.", buttons: ["Custom Order", "Available Inventory"], secAction: null, wallRoot: "modelS",
     }, 
     {
-        title: "Solar and Powerwall", description: "Power Everything", buttons: ["Learn More"], secAction: null
+        title: "Solar and Powerwall", description: "Power Everything", buttons: ["Learn More"], secAction: null, wallRoot: "solar",
     }, 
     {
-        title: "Model X", description: "0% Company Car Tax.", buttons: ["Custom Order", "Available Inventory"], secAction: null
+        title: "Model X", description: "0% Company Car Tax.", buttons: ["Custom Order", "Available Inventory"], secAction: null, wallRoot: "modelX",
     }, 
     {
-        title: "Model Y", description: "", buttons: ["Learn More"], secAction: null
+        title: "Model Y", description: "", buttons: ["Learn More"], secAction: null, wallRoot: "modelY",
     },
     {
-        title: "Accessories", description: "", buttons: ["Shop Now"], secAction: null
+        title: "Accessories", description: "", buttons: ["Shop Now"], secAction: null, wallRoot: "accessories",
     }
 ];
 
@@ -34,7 +38,7 @@ const HeroPage = (props: HeroProps): JSX.Element => {
     const heroPage = useRef<HTMLDivElement>(null);
     return (
         <>
-        <div className="hero-page" id={props.index.toString()} ref={heroPage}>
+        <div className="hero-page" id={props.index.toString()} ref={heroPage} style={{backgroundImage: "url(images/" + data.wallRoot + "-large.jfif)"}}>
             <Title title={data.title} description={data.description}></Title>
             <ActionButton actions={data.buttons}></ActionButton>
             <div className="sec-action">
@@ -45,7 +49,7 @@ const HeroPage = (props: HeroProps): JSX.Element => {
                 )}
             </div>
         </div>
-        <hr></hr>
+        
         </>
     );
 }
